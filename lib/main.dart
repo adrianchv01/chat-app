0b3c7cec6383b2cdf19e9cf791c28a9f19d7e950
adrianchv01 <adrianchv04@gmail.com>
2025-02-11 20:18:01 -0500

    Chat app

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:chat_app/config/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme( selectedColor: 2 ).theme(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: (){}, 
+            child: const Text('Click aqui')
+            )
+        ),
+      ),
+    );
+  }
+}
